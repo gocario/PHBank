285be93692a69d1e2317fbb0745a771f5a586701
Gocario <Gocario.dev@gmail.com>
2016-01-02 14:40:08 +0100

    Master-Manager clean

--- include/phbank.hpp
+++ include/phbank.hpp
@@ -5,26 +5,6 @@
 #include "save_manager.hpp"
 #include "data_manager.hpp"
 #include "font_manager.hpp"
-/*
-class PHBank
-{
-	public:
-		explicit PHBank();
-		~PHBank();
-
-		static PHBank* pHBank();
-		static PKBank* pKBank();
-		static sftd_font* font();
-
-		static Result load();
-		static Result destroy();
-
-	private:
-		static PHBank _pHBank;
-		static PKBank _pKBank;
-		static sftd_font* _font;
-};
-*/
 
 namespace PHBanku
 {
@@ -32,6 +12,5 @@ namespace PHBanku
 	extern DataManager* data;
 	extern FontManager* font;
 }
-
 
 #endif // PHBANK_HPP
