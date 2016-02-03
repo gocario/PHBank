19cf1ac69060c2a685fc4bfdd7041a6eb80fcb7e
Gocario <Gocario.dev@gmail.com>
2016-02-03 15:31:09 +0100

    Rewrite the texture_manager
    for a future thread for the loading screen.

--- include/texture_manager.hpp
+++ include/texture_manager.hpp
@@ -6,25 +6,28 @@
 #ifndef TEXTURE_MANAGER_HPP
 #define TEXTURE_MANAGER_HPP
 
-#include <3ds/types.h>
 #include <sf2d.h>
 
 class TextureManager
 {
 	public:
-		sf2d_texture* pkmIcons;
-		sf2d_texture* pkmShinyIcons;
-		sf2d_texture* ballIcons;
-		sf2d_texture* types;
-		sf2d_texture* boxTiles;
-		sf2d_texture* boxBackground;
-		sf2d_texture* resumeBackground;
+		sf2d_texture* ballLoadingScreen;
+		sf2d_texture* pkmSprites;		///< The sprite sheet for Pokémon 2D art.
+		sf2d_texture* pkmShinySprites;	///< The sprite sheet for shiny Pokémon 2D art.
+		sf2d_texture* pkmIcons;			///< The sprite sheet for Pokémon icons.
+		sf2d_texture* pkmShinyIcons;	///< The sprite sheet for shiny Pokémon icons.
+		sf2d_texture* itemIcons;		///< The sprite sheet for item icons.
+		sf2d_texture* ballIcons;		///< The sprite sheet for ball icons.
+		sf2d_texture* types;			///< The sprite sheet for lang types images.
+		sf2d_texture* boxTiles;			///< The sheet for box sprites (cursor, button, etc).
+		sf2d_texture* boxBackground;	///< The background of the box (TODO: Multiple background).
+		sf2d_texture* resumeBackground;	///< The resume background for top screen.
 		
 	public:
-		TextureManager();
-		~TextureManager();
+		TextureManager(void);
+		~TextureManager(void);
 
-		Result load();
+		Result load(void);
 };
 
 #endif // TEXTURE_MANAGER_HPP
