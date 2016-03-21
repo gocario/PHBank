c355caa62cee842dc882a26aa28550184faffaa6
Gocario <Gocario.dev@gmail.com>
2016-03-21 22:57:31 +0100

    Structurize the PersonalInfo

--- include/personal.hpp
+++ include/personal.hpp
@@ -7,26 +7,29 @@
 #define PERSONAL_INFO_XY_SIZE (0x40)
 #define PERSONAL_INFO_AO_SIZE (0x50)
 
-class PersonalInfo
+struct PersonalInfo
 {
-	public:
-		u8 HP, ATK, DEF, SPE, SPA, SPD;
-		u16 BST;
-		u8 types[2];
-		u16 items[3];
-		u8 gender, baseFriendship, expGrowth;
-		u16 formStats, formSprite;
-		u8 formCount, color;
-
-	public:
-		PersonalInfo(void);
-		void import(const u8* buffer);
+	uint8_t HP;
+	uint8_t ATK;
+	uint8_t DEF;
+	uint8_t SPE;
+	uint8_t SPA;
+	uint8_t SPD;
+	uint8_t types[2];
+	uint16_t items[3];
+	uint8_t gender;
+	uint8_t baseFriendship;
+	uint8_t expGrowth;
+	uint16_t formStats;
+	uint8_t formCount;
+	uint8_t color;
 };
 
 class PersonalMaster
 {
 	private:
-		u16 personalCount;
+		u32 personalCount;
+		u32 personalSize;
 		PersonalInfo* personals;
 
 	public:
--- source/personal.cpp
+++ source/personal.cpp
@@ -1,40 +1,34 @@
 #include "personal.hpp"
 
-#include <stdlib.h>
+// #include <stdlib.h>
 
-PersonalInfo::PersonalInfo(void)
-{
-
-}
-
-void PersonalInfo::import(const u8* buf)
+static void PersonalInfo_Import(PersonalInfo* pInfo, const u8* buf)
 {
 	if (!buf) return;
 
-	HP = buf[0x0];
-	ATK = buf[0x1];
-	DEF = buf[0x2];
-	SPE = buf[0x3];
-	SPA = buf[0x4];
-	SPD = buf[0x5];
-	BST = HP + ATK + DEF + SPE + SPA + SPD;
+	pInfo->HP = buf[0x0];
+	pInfo->ATK = buf[0x1];
+	pInfo->DEF = buf[0x2];
+	pInfo->SPE = buf[0x3];
+	pInfo->SPA = buf[0x4];
+	pInfo->SPD = buf[0x5];
 
-	types[0] = buf[0x6];
-	types[1] = buf[0x7];
+	pInfo->types[0] = buf[0x6];
+	pInfo->types[1] = buf[0x7];
 
-	items[0] = *(u16*)(buf+0xc);
-	items[1] = *(u16*)(buf+0xe);
-	items[2] = *(u16*)(buf+0x10);
-	gender = buf[0x12];
-	baseFriendship = buf[0x14];
-	expGrowth = buf[0x15];
+	pInfo->items[0] = *(u16*)(buf+0xc);
+	pInfo->items[1] = *(u16*)(buf+0xe);
+	pInfo->items[2] = *(u16*)(buf+0x10);
+	pInfo->gender = buf[0x12];
+	pInfo->baseFriendship = buf[0x14];
+	pInfo->expGrowth = buf[0x15];
 
-	formStats = *(u16*)(buf+0x1c);
-	formSprite = *(u16*)(buf+0x1e);
-	formCount = buf[0x20];
-	color = buf[0x21];
+	pInfo->formStats = *(u16*)(buf+0x1c);
+	pInfo->formCount = buf[0x20];
+	pInfo->color = buf[0x21];
+
+	// memcpy(pInfo, buf, sizeof(PersonalInfo));
 }
-
 
 PersonalMaster::PersonalMaster(void) : personals(NULL)
 {
@@ -49,10 +43,11 @@ PersonalMaster::~PersonalMaster(void)
 void PersonalMaster::import(const u8* buffer, u32 pCount, u32 pSize)
 {
 	personalCount = pCount;
-	personals = new PersonalInfo[personalCount];
-	for (u16 i = 0; i < personalCount; i++)
+	personalSize = pSize;
+	personals = new PersonalInfo[pCount];
+	for (u16 i = 0; i < pCount; i++)
 	{
-		personals[i].import((u8*)(buffer+pSize*i));
+		PersonalInfo_Import(&personals[i], (u8*)(buffer+pSize*i));
 	}
 }
 
